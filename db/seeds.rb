666f40d518d4fb9e63bab6057c03250cfe81750c
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-24 10:42:10 +0800

    + Added new sample seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+# . . .
+
+for i in 0..5
+
+Product.create!(title: 'POKEMON PLAMO COLLECTION NO.45 SELECT SERIES GENGAR ' + (i+1).to_s,
+description:
+%{<p>
+<strong>This is an injection-plastic kit of a science-fiction/anime item.</strong>
+</p>
+<p>
+The popular ghost type Pokemon Gengar is now available in Bandai's Pokemon Plamo Select Series! In addition to touch gate and snap fit technology, the number of stickers have been greatly reduced, and assembly is easy! Gengar comes with movable joints for posing and different facial and mouth parts. Catch this Pokemon for your collection today!
+</p>
+<p>
+    <strong>[Set Contents]</strong>
+</p>
+<ul>
+    <li>Face parts x 2</li>
+    <li>Tongue parts x 2</li>
+    <li>Foil seal x 2</li>
+</ul>
+},
+image_url: 'https://www.hlj.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/b/a/bans60441_0.jpg',
+price: 422.42)
+# . . .
+
+end
