d27d8e8aaa64738ad12e8b20e706cd7876b69125
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-25 16:55:10 +0800

    + Added counter for when the user visits the store index + Added some styling to the counter + Added the "Add to cart button" to products in the store list

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,8 @@ class StoreController < ApplicationController
   def index
 
     @products = Product.order(:title);
+    
+    @counter = session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1;
 
   end
 end
