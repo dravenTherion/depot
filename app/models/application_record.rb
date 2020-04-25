e9c7418005d24463774968c8efc6c79df491110f
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-25 17:00:23 +0800

    + Moved validation codes from ApplicationRecord to Product

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-
-  validates :title, length: {minimum:10, message: "must be 10 characters or longer"}
-  validates :title, uniqueness: true
-  
-  validates :description, :image_url, presence: true
-  validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.'}
-  
-  validates :price, numericality: {greater_than_or_equal_to: 0.01, message: "must be a valid amount"}
-
-
   self.abstract_class = true
 end
