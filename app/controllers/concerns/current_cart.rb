3ffd164f8ff29fe2bdee17bf04540d96bba4224d
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-25 18:31:17 +0800

    + moved cart owner validation to current_cart module

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -12,5 +12,21 @@ module CurrentCart
     
     end
 
+    # Redirect invalid cart to store index
+    def invalid_cart
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_index_url, notice: 'Invalid cart'
+    end
+    
+    # Redirect invalid owner to store index
+    def invalid_owner
+        
+        if(session[:cart_id].to_i != params[:id].to_i)
+            logger.error "Attempt to access invalid cart #{params[:id]}"
+            redirect_to store_index_url, notice: 'Invalid cart'
+        end
+
+    end
+
 end
     
