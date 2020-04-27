c9054e57aeb298a38906b5e9b37bdfe40ea3f611
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-28 06:00:19 +0800

    + skip authorization filter for store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,13 @@
 class StoreController < ApplicationController
   include CurrentCart
+  skip_before_action :authorize
   before_action :set_cart
+  
   def index
 
     @products = Product.order(:title);
     @counter = session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1;
 
   end
+
 end
