f8de076b4734e0c85393d59f054ccdad87ed24ec
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-27 12:23:30 +0800

    + added new product seed valus + removed error causing line in new.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
+
+
+
+## PRODUCT SEED ##
+
 Product.delete_all
 # . . .
 
-for i in 0..5
+#for i in 0..5
 
-Product.create!(title: 'POKEMON PLAMO COLLECTION NO.45 SELECT SERIES GENGAR ' + (i+1).to_s,
+Product.create!(title: 'POKEMON PLAMO COLLECTION NO.45 SELECT SERIES GENGAR ', #+ (i+1).to_s,
 description:
 %{<p>
 <strong>This is an injection-plastic kit of a science-fiction/anime item.</strong>
@@ -24,4 +29,53 @@ image_url: 'https://www.hlj.com/media/catalog/product/cache/image/700x700/e9c397
 price: 422.42)
 # . . .
 
-end
+Product.create!(title: '1/144 HG GUNDAM HAJIROBOSHI', #+ (i+1).to_s,
+description:
+%{<p>
+    <strong>
+        <span>
+            This is                             </span>
+        a posable, high-grade or better injection-plastic kit of an item from the Gundam universe.                        </strong>
+</p>
+            <div class="productdescription"><p>The leading mobile suit that appears in the upcoming mobile game "Iron-Blooded Orphans Urdr Hunt" is now an HG kit from Bandai! It features a shield with a variable claw that expands to the left and right, and a gimmick that shoots the needle in the center; the thruster has a structure in which two huge armor units swing left and right. A rifle is also included. Get this new mobile suit for your own collection today!</p><p><br></p><ul>
+<li>
+<strong>[Includes]</strong>: Mace, rifle, shield, foil seals</li></ul>
+},
+image_url: 'https://www.hlj.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/b/a/bans60424_0.jpg',
+price: 645.24)
+# . . .
+
+Product.create!(title: 'MAFEX MAGNETO (COMIC VER.)', #+ (i+1).to_s,
+description:
+%{<p>
+    <strong>
+        <span>
+            This is                             </span>
+        a posable action figure.                        </strong>
+</p>
+            <div class="productdescription"><p>As seen in the 1995 Marvel Comics crossover series "X-Men: Age of Apocalypse," here's the mighty Magneto, with long hair and his trademark cloak and helmet! His fabric cloak features a wire in the hem for those dramatic windblown poses, and he comes with three interchangeable facial expressions: with his helmet removed, with his helmet on and a "Grr!" expression, and with his helmet on and a darkened face with white eyes. Like all MAFEX figures, he's incredibly posable and comes with plenty of interchangeable hands, too. A stand with a movable arm is also included. Get this formidable character for your own collection today!</p><ul>
+<li>
+<strong>[Figure Size]</strong>: Approximately 16cm tall</li>
+</ul><p><br></p><p>
+
+
+<strong>[Set Contents]</strong>:</p><ul><li>Main figure</li><li>Interchangeable facial expressions (x3)</li><li>Effect parts (x2)</li><li>Stand with posing arm</li></ul>},
+image_url: 'https://www.hlj.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/m/e/medmafex-128_0.jpg',
+price: 3552.63)
+# . . .
+
+Product.create!(title: '1/24 HEXA GEAR STEELRAIN', #+ (i+1).to_s,
+description:
+%{<p>
+    <strong>
+        <span>
+            This is                             </span>
+        an injection-plastic kit of a science-fiction/anime item.                        </strong>
+</p>
+            <div class="productdescription"><p>Kotobukiya brings us their newest HEXA GEAR with the Steelrain! This newest addition features three main transformation modes: helicopter, vehicle, or humanoid mode. The cockpit can hold a Governor of your choice.&nbsp;The seat-unit inside the cockpit has a reclining and sliding feature to adjust to the size of the models being placed inside. The&nbsp;highly detailed cockpit has two types of adjustable levers and adjustable foot pedals installed so that users can fully enjoy arranging the outstanding Governors embarking/disembarking the helicopter.</p><p>The included 20mm Gatling Gun can be taken apart and used separately, similar to the Heavy Weapon Units. Also, it's compatible with the M.S.G Belt Links to enhance overall firepower. It's also compatible with the separately sold Flying Base Neo to create a dramatic display. Order now!</p><p><b>[Set Contents]</b></p><ul><li>Steelrain Model set</li><li>20mm Gatling Gun x2</li><li>Air-to-Surface Missiles x2</li><li>Rocket Launcher x2</li><li>Rocket Launcher Expansion Parts 2 sets</li><li>Archive Card x1</li></ul>
+},
+image_url: 'https://www.hlj.com/media/catalog/product/cache/image/700x700/e9c3970ab036de70892d86c6d221abfe/k/b/kbyhg-055_0.jpg',
+price: 4215.58)
+# . . .
+
+#end
