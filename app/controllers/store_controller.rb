02a71460e3cc34e928aa779d0ed12bc13ebce521
dravenTherion <i.am.quitoriano@gmail.com>
2020-04-28 20:27:35 +0800

    + Added handlers for locale parameters

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,8 +5,17 @@ class StoreController < ApplicationController
   
   def index
 
-    @products = Product.order(:title);
     @counter = session[:counter].nil? ? session[:counter] = 1 : session[:counter] += 1;
+
+    if params[:set_locale]
+      
+      redirect_to store_index_url(locale: params[:set_locale])
+    
+    else
+
+      @products = Product.order(:title);
+    
+    end
 
   end
 
